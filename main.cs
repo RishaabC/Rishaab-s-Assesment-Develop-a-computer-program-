798706c3644a1eaa38338fac8cf7c48bbfce2e3d
Rishaab Chandra <>
2023-03-26 01:19:12 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //Prompts User with Prior Instructions to  Quiz
+    Console.WriteLine ("Hello. Welcome to Mrs X's Year 9 Math Class. This quiz is to refresh your memory on some of our topics.");
+     Console.WriteLine();
+     Console.WriteLine ("There are 15 questions with 3 sections. Number, Area and Algebra. You will get 2 attempts per question. Good Luck!");
+      Console.WriteLine();
+
+      
+    //Compares and Checks User Input to Answer
+    //Right and Wrong Counter(Rounds Per Question)
+    //Random 5 Number Question Generator
+    //Random 5 A/P Question Generator
+    //5 Algbera Questions
+    //End Screen
+    
+      
+  }
+}
