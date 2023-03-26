ea3d54d0445fd4126cd147dd65d0147b15a45232
Rishaab Chandra <128944772+RishaabC@users.noreply.github.com>
2023-03-26 22:30:25 +0000

    Initial User Prompt

--- main.cs
+++ main.cs
@@ -8,11 +8,10 @@ class Program {
      Console.WriteLine ("There are 15 questions with 3 sections. Number, Area and Algebra. You will get 2 attempts per question. Good Luck!");
       Console.WriteLine();
 
-      
-    //Compares and Checks User Input to Answer
-    //Right and Wrong Counter(Rounds Per Question)
     //Random 5 Number Question Generator
     //Random 5 A/P Question Generator
+    //Compares and Checks User Input to Answer
+    //Right and Wrong Counter(Rounds Per Question)
     //5 Algbera Questions
     //End Screen
     
