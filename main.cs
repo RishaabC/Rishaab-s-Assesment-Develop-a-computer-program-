02c57f377517b89e408040052b3934b7be9adef2
Rishaab Chandra <128944772+RishaabC@users.noreply.github.com>
2023-03-30 02:41:00 +0000

    5 Random Algebra Question Generator

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ class Program
         //
         //`
 
-        Console.WriteLine("Hello. Welcome to Mrs X's Year 9 Math Class. This quiz is to refresh and test your mental math on some of our topics.");
+        Console.WriteLine("Hello. Welcome to Mrs X's Year 9 Math Class. This quiz is to refresh and test your mental maths on some of our topics.");
         Console.WriteLine();
         Console.WriteLine("There are 15 questions with 3 sections. Number, Area and Algebra. You will get 2 attempts per question. Good Luck!");
         Console.WriteLine();
@@ -29,18 +29,14 @@ class Program
         //
         //
 
-        //Number Variables
-        int num1 = 0;
-        int num2 = 0;
-
         //For Loop for 5 Questions
         for (int i = 1; i < 6; i++)
         {
             //Random Number Generator
             Random rndNums = new Random();
             //returns random numbers < 40
-            num1 = rndNums.Next(10, 50);
-            num2 = rndNums.Next(50, 100);
+            int num1 = rndNums.Next(10, 50);
+            int num2 = rndNums.Next(50, 100);
 
             //totals num1 and num2
             int correctAns = num1 + num2;
@@ -57,7 +53,7 @@ class Program
             numAns = 0;
             isNumber = true;
 
-            //Trys to convert input to number. 
+            //Tries to convert input to number. 
             try
             {
                 numAns = Convert.ToInt32(userInput);
@@ -78,7 +74,7 @@ class Program
                     score++;
                     Console.WriteLine();
                 }
-                //if answer is inncorrect
+                //if answer is incorrect
                 else if (numAns != correctAns && i != 5)
                 {
                     Console.WriteLine($"Incorrect, Answer is {correctAns}");
@@ -110,7 +106,7 @@ class Program
 
         //
         //
-        //Random 5 A/P Question Generator:
+        //Random 5 Area/Perimeter Question Generator:
         //
         //
 
@@ -120,29 +116,29 @@ class Program
         {
             Random rndNums2 = new Random();
             // Random Shape and Operator Picker
-            // Random Shape(1 = sqaure, 2 = rectangle, 3 = triangle)
+            // Random Shape(1 = square, 2 = rectangle, 3 = triangle)
             int shape = rndNums2.Next(1, 4);
             // Random operator(1 =  Area, 2 = Perimeter)
             int operation = rndNums2.Next(1, 3);
 
             // Random Length and Height Picker
             // Picks random value between 5 and 30 for both
-            int length = rndNums2.Next(5, 30);
-            int height = rndNums2.Next(5, 30);
-            //Third side for triangle
-            int sideC = rndNums2.Next(5, 30);
+            int length = rndNums2.Next(5, 25);
+            int height = rndNums2.Next(5, 25);
+            //Third side for triangle(if asked)
+            int sideC = rndNums2.Next(5, 25);
 
             switch (shape)
             {
                 // Square   
                 case 1:
-                    // Sqaure Area
-                    //Operation 1 means Area, This is choosen at random
+                    // Square Area
+                    //Operation 1 means Area, This is chosen at random
                     if (operation == 1)
                     {
                         //Prompts Question
                         Console.WriteLine($"{j}. Find the Area of a Square with the length and height of {length}?(Answer in numbers ONLY)");
-                        //Finds Sqaure Area
+                        //Finds Square Area
                         int correctAns = length * length; 
                         //Gets User Input
                         int userInput2 = Convert.ToInt32(Console.ReadLine());
@@ -154,14 +150,14 @@ class Program
                             score++;
                             Console.WriteLine();
                         }
-                        //If userInput isnt correct then it tells the user
+                        //If userInput isn't correct then it tells the user
                         else
                         {
                             Console.WriteLine($"Incorrect, The area of a Square is {correctAns}");
                             Console.WriteLine();
                         }
                     }
-                    // Sqaure Perimeter
+                    // Square Perimeter
                     else
                     {
                         //Prompts Question
@@ -178,7 +174,7 @@ class Program
                             score++;
                             Console.WriteLine();
                         }
-                        //If userInput is inncorrect then tells user
+                        //If userInput is incorrect then tells user
                         else
                         {
                             Console.WriteLine($"Incorrect, The perimeter of a Square is {correctAns}");
@@ -267,7 +263,7 @@ class Program
                     else
                     {
                         //Prompts Question
-                        Console.WriteLine($"{j}. Find the Perimeter of a Triangle with the side length's of {length},{height} and {sideC}?(Answer in numbers ONLY)");
+                        Console.WriteLine($"{j}. Find the Perimeter of a Triangle with the side lengths of {length},{height} and {sideC}?(Answer in numbers ONLY)");
                         //Calculates Triangle Area
                         int correctAns = length + height + sideC;
                         //Gets and Converts User Input
@@ -280,7 +276,7 @@ class Program
                             score++;
                             Console.WriteLine();
                         }
-                        //If answer is incorrect, tells user.
+                        //If the answer is incorrect, tell the user.
                         else
                         {
                             Console.WriteLine($"Incorrect, The perimeter of a Rectangle is {correctAns}");
@@ -290,7 +286,7 @@ class Program
                     break;
                 }
                 
-                //Notify's When moving to next section(Happens last)
+                //Notify When moving to next section(Happens last)
                 if(j == 10)
                 {
                     Console.WriteLine($"We will now be moving to the next section");
@@ -299,9 +295,125 @@ class Program
             
             }
 
+        //
+        //
+        //5 Random Algebra Questions Generator:
+        //
+        //
+
+        for (int k = 11; k < 16; k++)
+        {
+            //Array Num Random Generator
+            //Random Variable
+            Random rndNums3 = new Random();
+            //Min and Max values for array
+            int minVal = 5;
+            int maxVal = 25;
+            //New array of 6
+            int[] numarr = new int[6];
+
+            //Assigns random values to array
+            for (int l = 0; l < numarr.Length; l++)
+            {
+                numarr[l] = rndNums3.Next(minVal, maxVal);
+            }
+
+            //Var which picks which index in array and take its value
+            int numpicker = rndNums3.Next(0, 6);
+            int numpicker2 = rndNums3.Next(0, 6);
+
+            //Randomly Get our first and second var
+            int firstVar = numarr[numpicker];
+            int secondVar = numarr[numpicker2];
 
 
-            //5 Algbera Questions
-            //End Screen
+            //Operation Picker
+            //1= +, 2 =-
+            int operationNum = rndNums3.Next(1, 3);
+
+
+            //Ask Question
+            switch (operationNum)
+            {
+                //Algebraic Addition:
+                case 1:
+                    //Prompts Question
+                    Console.WriteLine($"{k}. What is {firstVar}x + {secondVar}x (Answer with numbers and letters ONLY)");
+                    //Find Addition of 2 Var's
+                    int additionAns = firstVar + secondVar;
+                    //Converts Correct Answer to a string
+                    string correctAns3 = additionAns + "x";
+                    //Gets user input
+                    string userInput3 = Console.ReadLine();
+                    //convert to lowercase and removes spaces
+                    userInput3 = userInput3.ToLower().Replace(" ", "");
+                    userInput3 = userInput3.Replace(" ", "");
+                    //Checks Answer, If correct:
+                    if (correctAns3 == userInput3)
+                    {
+                        Console.WriteLine($"Correct, Answer is {correctAns3}");
+                        score++;
+                        Console.WriteLine();
+                    }
+                    //If Incorrect:
+                    else
+                    {
+                        Console.WriteLine($"Incorrect, Answer is {correctAns3}");
+                        Console.WriteLine();
+                    }
+                    break;
+                //Algebraic Subtraction:
+                case 2:
+                    //Prompts Question
+                    Console.WriteLine($"{k}. What is {firstVar}x - {secondVar}x (Answer with numbers and letters ONLY)");
+                    //Find difference of 2 Var's
+                    int minusAns = firstVar - secondVar;
+                    //Converts Correct Answer to a string
+                    string correctAns4 = minusAns + "x";
+                    //Gets user input
+                    string userInput4 = Console.ReadLine();
+                    //convert to lowercase and removes spaces
+                    userInput4 = userInput4.ToLower();
+                    userInput4 = userInput4.Replace(" ", "");
+                    //Checks Answer, If correct:
+                    if (correctAns4 == userInput4)
+                    {
+                        Console.WriteLine($"Correct, Answer is {correctAns4}");
+                        score++;
+                        Console.WriteLine();
+                    }
+                    //If person answer x or 0 instead of 1x or 0x
+                    else if (minusAns == 0 || minusAns == 1)
+                    {
+                        if (userInput4 == "x" || userInput4 == "0")
+                        {
+                            Console.WriteLine($"Correct, Answer is {minusAns}");
+                            //score++;
+                            Console.WriteLine();
+                        }
+                    }
+                    //If Incorrect:
+                    else
+                    {
+                        Console.WriteLine($"Incorrect, Answer is {correctAns4}");
+                        Console.WriteLine();
+                    }
+
+                    break;
+
+            }
+            //Notify When 5 questions are done
+                if(k == 15)
+                {
+                    Console.WriteLine($"We have now finished. Well Done. Here are the results");
+                            Console.WriteLine();
+                }
+        }
+                                
+        //
+        //
+        //End Screen:
+        //
+        //
     }
 }
